802dcaf3eda604ebd998884d79ad21576ceb3b8f
xaratustrah <xaratustrah@users.noreply.github.com>
2025-04-04 01:11:25 +0200

    npz addup

--- npz_addup.py
+++ npz_addup.py
@@ -2,6 +2,13 @@ import argparse
 import numpy as np
 from loguru import logger
 from tqdm import tqdm
+from iqtools import *
+import sys
+
+# font settings for plot
+font = {"weight": "bold", "size": 6}  #'family' : 'normal',
+plt.rc("font", **font)
+
 
 def process_files(file_list):
     """
@@ -10,19 +17,21 @@ def process_files(file_list):
     zz_sum = None
     found_files = False
 
-    with open(file_list, 'r') as f:
+    with open(file_list, "r") as f:
         files = f.read().splitlines()
 
     spectrogram_files = [file for file in files if file.endswith("_spectrogram.npz")]
 
     if not spectrogram_files:
-        logger.info("No files ending with '_spectrogram.npz' found. Exiting gracefully.")
+        logger.info(
+            "No files ending with '_spectrogram.npz' found. Exiting gracefully."
+        )
         return zz_sum, False
 
     for file in tqdm(spectrogram_files, desc="Processing files"):
         try:
             data = np.load(file)
-            xx, yy, zz = data['arr_0'], data['arr_1'], data['arr_2']
+            xx, yy, zz = data["arr_0"], data["arr_1"], data["arr_2"]
             if zz_sum is None:
                 zz_sum = zz
             else:
@@ -31,23 +40,72 @@ def process_files(file_list):
         except Exception as e:
             logger.error(f"Error processing file {file}: {e}")
 
-    return xx, yy, zz_sum
+    return xx, yy, zz_sum, found_files
+
 
 def main():
     """
     Main function to parse arguments and invoke the processing function.
     """
-    parser = argparse.ArgumentParser(description="Process a file list and sum the 'zz' arrays from spectrogram files.")
-    parser.add_argument("file_list", type=str, help="Path to the file containing the list of file paths.")
+    parser = argparse.ArgumentParser(
+        description="Process a file list and sum the 'zz' arrays from spectrogram files."
+    )
+    parser.add_argument(
+        "file_list",
+        type=str,
+        help="Path to the file containing the list of file paths.",
+    )
     args = parser.parse_args()
 
-    xx, yy, zz = process_files(args.file_list)
-    print(np.shape(xx), np.shape(yy), np.shape(zz))
+    try:
+        logger.info("Starting the summation...")
+        xx, yy, zz_sum, found_files = process_files(args.file_list)
+        navg = np.shape(xx)[0]
 
-    if found_files:
-        logger.info("Successfully processed all spectrogram files and summed the 'zz' arrays.")
-    else:
-        logger.info("No valid spectrogram files found to process.")
+        logger.info("Saving 3D NPZ sum to file...")
+        np.savez("summed_spectrogram.npz", arr_0=xx, arr_1=yy, arr_2=zz_sum)
+
+        logger.info("Creating 2D average...")
+        xx_avg, yy_avg, zz_sum_avg = get_averaged_spectrogram(xx, yy, zz_sum, every=navg)
+
+        logger.info("Saving 2D NPZ sum to file...")
+        np.savez("summed_spectrum.npz", arr_0=xx_avg.flatten(), arr_1=zz_sum_avg.flatten())
+
+        logger.info("Plotting the 2D NPZ sum...")
+        plot_spectrum(
+            xx_avg.flatten(),
+            zz_sum_avg.flatten(),
+            dbm=True,
+            filename="summed_spectrum",
+            title="summed_spectrum",
+        )
+
+        logger.info("Plotting the 3D NPZ sum...")
+        slx = slice (int(np.shape(xx)[1]/2) - 500, int(np.shape(xx)[1]/2) + 500)
+        sly = slice(0,np.shape(yy)[0])
+        plot_spectrogram(
+            xx[sly,slx], yy[sly,slx], zz_sum[sly,slx],
+            zzmin=10,
+            zzmax=500,
+            filename="summed_spectrogram",
+            title="summed_spectrogram",
+        )
+
+
+        if found_files:
+            logger.info(
+                "Successfully processed all spectrogram files."
+            )
+        else:
+            logger.info("No valid spectrogram files found to process.")
+    except KeyboardInterrupt:
+        logger.warning("Process interrupted by user (Ctrl+C). Exiting gracefully.")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        logger.warning("Script interrupted (Ctrl+C). See you!")
+        sys.exit(1)
